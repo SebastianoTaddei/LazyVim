d5d40dd38dd90069875f10f681ef890d98bcd919
Sebastiano Taddei <sebastianotaddei@gmail.com>
2024-03-03 15:29:37 +0100

    push

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add atom one dark
+  { "navarasu/onedark.nvim" },
+
+  -- Configure LazyVim to load the colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
