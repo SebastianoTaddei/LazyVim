a799abf6c5a6cbc780596cac788a9942dd27d8bb
Sebastiano Taddei <61469576+SebastianoTaddei@users.noreply.github.com>
2025-04-22 09:15:10 +0200

    Fixed clangd config + added latex

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,8 +1,21 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		opts = {
-			inlay_hints = { enabled = false },
-		},
-	},
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+      servers = {
+        clangd = {
+          cmd = {
+            "/opt/homebrew/opt/llvm/bin/clangd",
+            "--background-index",
+            "--clang-tidy",
+            "--header-insertion=iwyu",
+            "--completion-style=detailed",
+            "--function-arg-placeholders",
+            "--fallback-style=llvm",
+          },
+        },
+      },
+    },
+  },
 }
---
+++ lua/plugins/vimtex.lua
@@ -0,0 +1,10 @@
+return {
+  "lervag/vimtex",
+  lazy = false,
+  init = function()
+    vim.g.vimtex_view_method = "skim"
+    vim.g.vimtex_compiler_latexmk = {
+      out_dir = "build",
+    }
+  end,
+}
