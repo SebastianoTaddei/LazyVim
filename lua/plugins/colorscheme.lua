37a8afae3c69f4ba6d9e22b378de6b53b78c33d3
Sebastiano Taddei <61469576+SebastianoTaddei@users.noreply.github.com>
2025-08-20 08:38:35 +0200

    Updates + fix to colorscheme breaking change

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,5 +8,16 @@ return {
     opts = {
       colorscheme = "catppuccin-macchiato",
     },
+    specs = {
+      {
+        "akinsho/bufferline.nvim",
+        init = function()
+          local bufline = require("catppuccin.groups.integrations.bufferline")
+          function bufline.get()
+            return bufline.get_theme()
+          end
+        end,
+      },
+    },
   },
 }
