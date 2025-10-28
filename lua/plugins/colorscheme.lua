3d56cdcb933aea7ff2e0bb3eac0614794f4fabc1
Sebastiano Taddei <61469576+SebastianoTaddei@users.noreply.github.com>
2025-10-28 15:43:06 +0100

    Updates

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,23 +1,9 @@
 return {
-  -- add onedarkpro
   { "catppuccin/nvim" },
-
-  -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin-macchiato",
-    },
-    specs = {
-      {
-        "akinsho/bufferline.nvim",
-        init = function()
-          local bufline = require("catppuccin.groups.integrations.bufferline")
-          function bufline.get()
-            return bufline.get_theme()
-          end
-        end,
-      },
     },
   },
 }
